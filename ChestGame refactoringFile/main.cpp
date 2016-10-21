2cc7bcf363c9559cd12d3b3cdd1601161443ef66
Kevin Kurashewich <kevinkurashewich.code@gmail.com>
2016-10-20 17:31:51 -0700

    Initial Commit

---
+++ ChestGame refactoringFile/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChestGame refactoringFile
+//
+//  Created by kevin kurashewich on 10/20/16.
+//  Copyright © 2016 Kevin Kurashewich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
